57e3fe50111cf2bb71e58470ec4c515b98cb628f
Mahsa <mahsa.ershadipour.81@gmail.com>
2025-05-20 12:23:53 +0330

    uploading photo

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
+const sharp = require('sharp');
 const { Photo, User } = require('../models/db');
 const db = require('../models/db');
 const authenticateUser = require('../Middleware/auth');
@@ -12,12 +13,18 @@ const { where } = require('sequelize');
 
 // Set up multer storage
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: function (req, file, cb) {
         cb(null, 'uploads/');
     },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname); 
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
     }
+});
+
+//File size limit
+const uploads = multer({
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 } //Maximum file size: 5MB
 });
 
 const upload = multer({ storage });
@@ -28,6 +35,7 @@ router.post('/upload', authenticateUser, upload.single('photo'), async (req, res
     const file = req.file;
 
     try {
+        //check for file existing
         if (!file) {
             return res.status(400).json({ error: 'No file uploaded' });
         }
@@ -44,11 +52,24 @@ router.post('/upload', authenticateUser, upload.single('photo'), async (req, res
             return res.status(409).json({ error: 'This image has already been uploaded by the same user.' });
         }
 
-        const newPhoto = await Photo.create({
-            filename: file.filename,
-            path: file.path,
-            user_id: userId
-        });
+        // Convert to .webp
+            const originalPath = file.path;
+            const webpFilename = path.parse(file.filename).name + '.webp';
+            const webpPath = path.join('uploads', webpFilename);
+
+            await sharp(originalPath)
+                .webp({ quality: 80 })
+                .toFile(webpPath);
+
+            // Optionally delete original file
+            fs.unlinkSync(originalPath);
+
+            // Save to DB
+            const newPhoto = await Photo.create({
+                filename: webpFilename,
+                path: webpPath,
+                user_id: userId
+            });
 
         res.status(201).json({ message: 'Photo uploaded successfully', photo: newPhoto });
     } catch (err) {
@@ -59,7 +80,7 @@ router.post('/upload', authenticateUser, upload.single('photo'), async (req, res
 
 
 //get all the upload photos
-router.get('/photos', async (req, res) => {
+router.get('/all-photos', async (req, res) => {
     try {
         const photos = await db.Photo.findAll({
                     where: {status: 1}, 
@@ -75,6 +96,33 @@ router.get('/photos', async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 })
+
+//get photos by userId in header
+router.get('/photos',authenticateUser, async (req, res) => {
+    console.log('Inside /photos route, userId', req.userId);
+    const userId = req.userId;
+
+    try {
+        // Check for user existence
+        const user = await db.User.findByPk(userId);
+        if (!user){
+            return res.status(404).json({ error: 'there is no user with this user id'});
+        }
+        
+        // Find user's photos
+        const photos = await db.Photo.findAll({
+            where: { user_id: userId, status: 1},
+            include: [
+                { model: db.User,as: 'Author', attributes: ['username'] }
+            ],
+            order: [['uploadedat', 'DESC']]
+        });
+        res.json(photos);
+    } catch (err) {
+        console.error('Error in /photos route',err);
+        res.status(500).json({ error: 'Server error'});
+    }
+});
 
 //delete a photo
 router.delete('/photos/:id', authenticateUser, async (req, res) => {
